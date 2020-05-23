27ea311243ddf2c65d157ce5efbb8c84e9b9963d
Chebesi Awah <chebesiawah@gmail.com>
2020-05-23 19:38:33 +0100

    home page on construction

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -10,9 +10,9 @@ const Icons = () => {
          
           <div className="side-icons">
               <li><a href="https://wwww.facebook.com" className="icons"><i className="fab fa-facebook fa-fw"></i> </a></li>
-              <li><a href="https://wwww.facebook.com" className="icons"><i className="fab fa-twitter fa-fw"></i> </a></li>
-              <li><a href="https://wwww.facebook.com" className="icons"><i className="fab fa-google-plus fa-fw"></i> </a></li>
-              <li><a href="https://wwww.facebook.com" className="icons"><i className="fab fa-instagram fa-fw"></i> </a></li>
+              <li><a href="https://wwww.twitter.com" className="icons"><i className="fab fa-twitter fa-fw"></i> </a></li>
+              <li><a href="https://wwww.google .com" className="icons"><i className="fab fa-google-plus fa-fw"></i> </a></li>
+              <li><a href="https://wwww.instagram.com" className="icons"><i className="fab fa-instagram fa-fw"></i> </a></li>
               </div>
               <div>
               <h3 className="texi">On The Road</h3>
--- src/components/home.js
+++ src/components/home.js
@@ -6,10 +6,22 @@ class Home extends Component {
     render() {
         return(
             <div>
-                
-            </div>
-        )
-    }
+           <div className="cover">
+      
+<h1 className="text">Wildest Dreamz!!</h1>
+  <h2 className="tex">Travel the World and Experiance its Wonders!!!</h2>
+
+<h2 className="tex">...Lets gooooo!!!!!</h2>
+      
+     </div>
+     <footer className="foot"><h4> Reel Adventures 2020&copy;</h4></footer>
+</div>
+      
+   
+    )
+
+
+}
 }
 
 
