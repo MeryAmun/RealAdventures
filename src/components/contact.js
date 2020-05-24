c5b40556029a1d44d27e8a62be4ef2ff3fe1627a
Chebesi Awah <chebesiawah@gmail.com>
2020-05-24 22:20:37 +0100

    Contact Page on construction

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,40 @@
 import React from 'react'
 
 
-const contact = () => {
-return (
-     <div>
-         
-     </div>
- )
+const Contact = () => {
+   //programatic ridirect
+    //setTimeout(() => {
+       // props.history.push('/')
+   // }, 5000);
+
+   return (
+    <div>
+    <div className="contact">
+
+    
+<form className="container">
+                   First Name:
+             <input type="text" name="name"/>
+            Last Name:
+             <input type="text" name="name"/>
+             Comment:
+           <textarea></textarea>
+           <button className="btn">Leave your comment</button>
+                </form>
+             
+            
+            <div className="side" />
+            <div className="contact-us" />
+             
+    
+    </div>
+    <footer className="foot">
+<h4> Reel Adventures 2020&copy;</h4></footer>  
+
+    </div>
+)
+
+
 }
 
-
-export default contact
+export default Contact
