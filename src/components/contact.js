3c78bc3b74f86f14c056cdef60e5376f5d82497a
Chebesi Awah <chebesiawah@gmail.com>
2020-05-26 20:23:35 +0100

    contact page done

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,20 +11,20 @@ const Contact = () => {
     <div>
     <div className="contact">
 
-    
-<form className="container">
-                   First Name:
-             <input type="text" name="name"/>
-            Last Name:
-             <input type="text" name="name"/>
-             Comment:
-           <textarea></textarea>
-           <button className="btn">Leave your comment</button>
-                </form>
-             
-            
-            <div className="side" />
-            <div className="contact-us" />
+            <div className="side"></div>
+            <div className="contact-us">
+            <h3>   
+ <i className="fas fa-envelope-open-text"></i>
+
+    chebesiawah@gmail.com
+                
+<i className="fas fa-mobile"></i>
+
+(+237) 677972126
+</h3>
+<a href="https://github.com/MeryAmun" target= "_blank" className="link" >Github:https://github.com/MeryAmun</a>
+
+                </div>
              
     
     </div>
