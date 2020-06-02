cd84a658a7ab1244c54a32dc9e20fe951da50fdf
Chebesi Awah <chebesiawah@gmail.com>
2020-06-02 20:07:38 +0100

    About page on construction

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
 
 
 class About extends Component {
 
     render() {
-        return (
-            <div>
+      //console.log(this.props)
+    return (
+            <div className="about">
+            <div className="abt" >
           
+             <div className="figure">
+                 <div className="section"> Reel Ad -ventures, is where you need to come to pick that perfect vacation spot. You are Welcome to our vacation resource bank,and we trust to be of excellent service to you.We recommend that before you click the destination button,share with our wonderful family the experiences from your last vacation we love travel stories :).click the button below to get started.
+                 
+                 <Link to={'/:todos/'}>
+                 <button className="btn" >Get Started</button></Link>  
+            
+           </div>
+           
+          <div className="anim"/>
+     
+         </div>
+      
+         </div>
+     
+     
+     
+     
+     
+                 <footer className="foot"><h4> Reel Adventures 2020&copy;</h4></footer>
+     
              </div>
          );
     }
