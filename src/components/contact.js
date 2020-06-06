ab3dd8c8151118ec552ba36aad1375241afd1f26
Chebesi Awah <chebesiawah@gmail.com>
2020-06-06 22:47:43 +0100

    query and fonts adjustments

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,20 +10,19 @@ const Contact = () => {
    return (
     <div>
     <div className="contact">
-
-            <div className="side"></div>
             <div className="contact-us">
             <h3>   
  <i className="fas fa-envelope-open-text"></i>
 
     chebesiawah@gmail.com
-     <div classsName="spacer" />           
+    <div className="spacer"/>
+               
 <i className="fas fa-mobile"></i>
 
 (+237) 677972126
-</h3>
+<div className="spacer"/>
 <a href="https://github.com/MeryAmun" target= "_blank" className="link" >Github:https://github.com/MeryAmun</a>
-
+</h3>
                 </div>
              
     
