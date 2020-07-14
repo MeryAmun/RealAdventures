d4e3bd94dabf9d9ddb83e38246f37a38c3c82c86
Chebesi Awah <chebesiawah@gmail.com>
2020-07-14 08:28:20 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-This project was built using npm create - react-app.After this installation,I proceeded to setup the various components.
-I installed react-router-dom to setup links and brouser router paths.Then I installed reduc,and react-redux to connect the react app to the redux store.
-Futhrermore I used Higher Order Components like connect to bind the state of the redux store to the props of ther component.
---
+++ precache-manifest.e9814056ebd14fe7482c548576f667bb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24060ae16bb5d32b409166aecee158c1",
+    "url": "/ReelAdventures/index.html"
+  },
+  {
+    "revision": "c86b96ca8e6f1a4942c0",
+    "url": "/ReelAdventures/static/css/main.65871f0f.chunk.css"
+  },
+  {
+    "revision": "ddad770631b5cdfc08eb",
+    "url": "/ReelAdventures/static/js/2.10e11b72.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/ReelAdventures/static/js/2.10e11b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86b96ca8e6f1a4942c0",
+    "url": "/ReelAdventures/static/js/main.1c14902b.chunk.js"
+  },
+  {
+    "revision": "3d423d4531f48221908b",
+    "url": "/ReelAdventures/static/js/runtime-main.3ad30457.js"
+  },
+  {
+    "revision": "886d6b8816e686396f9399d429440bba",
+    "url": "/ReelAdventures/static/media/6.886d6b88.jpg"
+  },
+  {
+    "revision": "abc60cd88d423a519980742e078a829c",
+    "url": "/ReelAdventures/static/media/cam2.abc60cd8.jpg"
+  },
+  {
+    "revision": "f93e4f8eec3fe2fc04f6c5c27a35c7d6",
+    "url": "/ReelAdventures/static/media/cam5.f93e4f8e.jpg"
+  },
+  {
+    "revision": "33f92b6a8066e12182cb18cea4f30e75",
+    "url": "/ReelAdventures/static/media/egy4.33f92b6a.jpg"
+  },
+  {
+    "revision": "23783002e70ffa75f69038567956abe6",
+    "url": "/ReelAdventures/static/media/jam1.23783002.jpg"
+  },
+  {
+    "revision": "1175ea5a7e465934a27eb328c52c4754",
+    "url": "/ReelAdventures/static/media/jam4.1175ea5a.jpg"
+  },
+  {
+    "revision": "c6c561a160f66b8ea916ee604971b336",
+    "url": "/ReelAdventures/static/media/ken1.c6c561a1.jpg"
+  },
+  {
+    "revision": "4e5cb31662e8fb20e3b42d43d255658f",
+    "url": "/ReelAdventures/static/media/ken5.4e5cb316.jpg"
+  },
+  {
+    "revision": "bc438183571d86d1f4efd0e45f6566c4",
+    "url": "/ReelAdventures/static/media/namib2.bc438183.jpg"
+  },
+  {
+    "revision": "ef0c85e1b2be6f3a1f2ae4d44ae0e61b",
+    "url": "/ReelAdventures/static/media/nc.ef0c85e1.jpg"
+  },
+  {
+    "revision": "b5ec43f766156a21e95cd1ac9df090a6",
+    "url": "/ReelAdventures/static/media/ni.b5ec43f7.jpg"
+  },
+  {
+    "revision": "1bceef99fa0950bc0441011867cc0edd",
+    "url": "/ReelAdventures/static/media/nr.1bceef99.jpg"
+  },
+  {
+    "revision": "9603f6df2bf5afe6fb7de08775555090",
+    "url": "/ReelAdventures/static/media/va4.9603f6df.jpeg"
+  }
+]);
